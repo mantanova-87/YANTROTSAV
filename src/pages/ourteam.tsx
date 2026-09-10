c6a1781532ba4982c211b16deb8235e33699da2e
mantanova-87 <cdt.mantavyakumar@gmail.com>
2026-09-10 22:17:11 +0530

    Refactor code structure for improved readability and maintainability

--- src/pages/ourteam.tsx
+++ src/pages/ourteam.tsx
@@ -8,7 +8,7 @@ import priyanshu from "../assets/images/priyanshu.jpeg";
 import sachin from "../assets/images/sachin.jpg";
 import abhinav from "../assets/images/abhinav.png";
 import Harsh from "../assets/images/Harsh.jpeg";
-
+import ankit from "../assets/images/ankitojha.jpeg"
 const teamMembers = [
   {
     name: "ABHINAV KUMAR",
@@ -63,6 +63,15 @@ const teamMembers = [
     linkedin: "https://www.linkedin.com/in/mehak-sharma-cyber",
     whatsapp: "https://wa.me/919906814723",
     image: mehak,
+  },
+  {
+    name: "ANKIT KUMAR OJHA",
+    designation: "",
+    email: "25BECSE15.cse@cujammu.ac.in",
+    instagram: "https://www.instagram.com/ankit_lalji",
+    linkedin: "https://www.linkedin.com/in/ankit-kumar-ojha-04de13d12m/",
+    whatsapp: "https://wa.me/919334569412",
+    image: ankit,
   },
 ];
 
